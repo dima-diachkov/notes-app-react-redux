70089fdcbd7e64911fb26d3e4c3171b539c16e44
dima-diachkov <diachkov.dima@gmail.com>
2023-07-27 12:09:05 +0200

    split table to active and archive notes, edit form

--- src/components/NoteTable.tsx
+++ src/components/NoteTable.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from './Table';
 import { useDispatch } from 'react-redux';
 import { Note } from '../types/types';
 import { editNote, removeNote, archiveNote, unarchiveNote } from '../actions/noteActions';
+import EditNoteForm from './EditNoteForm';
 
 interface NoteTableProps {
   notes: Note[];
@@ -10,32 +11,14 @@ interface NoteTableProps {
 
 const NoteTable: React.FC<NoteTableProps> = ({ notes }) => {
   const dispatch = useDispatch();
-  const tableHeaders = ['Time of Creation', 'Note Content', 'Note Category', 'Mentioned Dates', 'Actions'];
+  const activeNotes = notes.filter((note) => !note.archived);
+  const archivedNotes = notes.filter((note) => note.archived);
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
 
-  const tableData = notes.map((note) => {
-    const mentionedDates = note.content.match(/\d{1,2}\/\d{1,2}\/\d{4}/g) || [];
-
-    return {
-      'Time of Creation': note.time,
-      'Note Content': note.content,
-      'Note Category': note.category,
-      'Mentioned Dates': mentionedDates.join(', '),
-      'Actions': (
-        <div>
-          <button onClick={() => handleEditNote(note)}>Edit</button>
-          <button onClick={() => handleRemoveNote(note.id)}>Remove</button>
-          {note.archived ? (
-            <button onClick={() => handleUnarchiveNote(note.id)}>Unarchive</button>
-          ) : (
-            <button onClick={() => handleArchiveNote(note.id)}>Archive</button>
-          )}
-        </div>
-      ),
-    };
-  });
-
-  const handleEditNote = (note: Note) => {
-    dispatch(editNote(note));
+  const handleEditNote = (updatedNote: Note) => {
+    dispatch(editNote(updatedNote));
+    handleCloseEditForm();
   };
 
   const handleRemoveNote = (noteId: number) => {
@@ -50,7 +33,67 @@ const NoteTable: React.FC<NoteTableProps> = ({ notes }) => {
     dispatch(unarchiveNote(noteId));
   };
 
-  return <Table headers={tableHeaders} data={tableData} />;
+  const handleEditButtonClick = (note: Note) => {
+    setSelectedNote(note);
+    setShowEditForm(true);
+  };
+
+  const handleCloseEditForm = () => {
+    setSelectedNote(null);
+    setShowEditForm(false);
+  };
+
+  return (
+    <div>
+      <h2>Active Notes</h2>
+      <Table
+        headers={['Time of Creation', 'Note Content', 'Note Category', 'Mentioned Dates', 'Actions']}
+        data={activeNotes.map((note) => {
+          const mentionedDates = note.content.match(/\d{1,2}\/\d{1,2}\/\d{4}/g) || [];
+
+          return {
+            'Time of Creation': note.time,
+            'Note Content': note.content,
+            'Note Category': note.category,
+            'Mentioned Dates': mentionedDates.join(', '),
+            'Actions': (
+              <div>
+                <button onClick={() => handleEditButtonClick(note)}>Edit</button>
+                <button onClick={() => handleRemoveNote(note.id)}>Remove</button>
+                <button onClick={() => handleArchiveNote(note.id)}>Archive</button>
+              </div>
+            ),
+          };
+        })}
+      />
+
+      <h2>Archived Notes</h2>
+      <Table
+        headers={['Time of Creation', 'Note Content', 'Note Category', 'Mentioned Dates', 'Actions']}
+        data={archivedNotes.map((note) => {
+          const mentionedDates = note.content.match(/\d{1,2}\/\d{1,2}\/\d{4}/g) || [];
+
+          return {
+            'Time of Creation': note.time,
+            'Note Content': note.content,
+            'Note Category': note.category,
+            'Mentioned Dates': mentionedDates.join(', '),
+            'Actions': (
+              <div>
+                <button onClick={() => handleEditButtonClick(note)}>Edit</button>
+                <button onClick={() => handleRemoveNote(note.id)}>Remove</button>
+                <button onClick={() => handleUnarchiveNote(note.id)}>Unarchive</button>
+              </div>
+            ),
+          };
+        })}
+      />
+
+      {showEditForm && selectedNote && (
+        <EditNoteForm note={selectedNote} onClose={handleCloseEditForm} />
+      )}
+    </div>
+  );
 };
 
 export default NoteTable;

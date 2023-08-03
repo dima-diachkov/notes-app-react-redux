b7ee9056905bd819185ab324523299135a69bbd5
dima-diachkov <diachkov.dima@gmail.com>
2023-08-03 19:03:26 +0200

    add NoteTable to storybook

---
+++ src/stories/NoteTable.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import NoteTable from '../components/NoteTable';
+import store from '../store';
+import { Provider } from 'react-redux';
+import defaultNotes from '../mocks/defaultNotes';
+
+const notes = [...defaultNotes];
+
+const meta = {
+    title: 'Example/NoteTable',
+    component: NoteTable,
+    tags: ['autodocs'],
+    decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+    argTypes: {
+        notes
+    },
+} satisfies Meta<typeof NoteTable>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        notes
+    },
+};
